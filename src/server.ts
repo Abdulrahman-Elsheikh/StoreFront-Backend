393a87f8043871aaeeefcf2d17ee34b184fc23c8
Abdulrahman Elsheikh <eng.abdulrahman.elsheikh@gmail.com>
2022-02-18 15:02:10 +0200

    Connect To Database

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,36 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import errorMiddleware from './middlewares/error.middleware';
+import config from './middlewares/config';
+import db from './database/database';
 
 const app: express.Application = express();
-const address = '0.0.0.0:3000';
+const PORT = config.port || 3000;
+const address = `0.0.0.0:${PORT}`;
 
 app.use(bodyParser.json());
 app.use(express.json());
 
 app.get('/', function (_req: Request, res: Response) {
-  throw new Error();
   res.send('Hello World!');
+});
+
+db.connect().then((client) => {
+  return client
+    .query('SELECT NOW()')
+    .then((res) => {
+      client.release();
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      client.release();
+      console.log(err.stack);
+    });
 });
 
 app.use(errorMiddleware);
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log(`starting app on: ${address}`);
 });
 
