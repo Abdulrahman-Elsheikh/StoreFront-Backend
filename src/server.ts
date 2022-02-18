55597758b2634bc77396ea3cdba36d74a91fc940
Abdulrahman Elsheikh <eng.abdulrahman.elsheikh@gmail.com>
2022-02-18 02:11:26 +0200

    Handling Errors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import errorMiddleware from './middlewares/error.middleware';
 
 const app: express.Application = express();
-const address: string = '0.0.0.0:3000';
+const address = '0.0.0.0:3000';
 
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', function (_req: Request, res: Response) {
+  throw new Error();
   res.send('Hello World!');
 });
+
+app.use(errorMiddleware);
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
--- src/tests/server.spec.ts
+++ src/tests/server.spec.ts
@@ -4,8 +4,8 @@ import app from '../server';
 const request = supertest(app);
 
 describe('Test basic endpoint server', () => {
-  it('Get the / endpoint', async () => {
+  it('Get the main endpoint', async () => {
     const response = await request.get('/');
-    expect(response.status).toEqual(200);
+    expect(response.status).toBe(200);
   });
 });
