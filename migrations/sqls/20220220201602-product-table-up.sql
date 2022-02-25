9027a4a2d0efe2c101f2c3dc665247364339edec
Abdulrahman Elsheikh <eng.abdulrahman.elsheikh@gmail.com>
2022-02-25 04:53:29 +0200

    Update Migrations

--- migrations/sqls/20220220201602-product-table-up.sql
+++ migrations/sqls/20220220201602-product-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    price integer NOT NULL,
+    price INTEGER NOT NULL,
     category VARCHAR(50) NOT NULL
 );
--- migrations/sqls/20220222202810-order-table-up.sql
+++ migrations/sqls/20220222202810-order-table-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE orders(
     id SERIAL PRIMARY KEY,
-    user_id integer NOT NULL REFERENCES users(id),
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     status VARCHAR(50) NOT NULL
 );
